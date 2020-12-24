c1f776252fa0267e0622cb50247f9f5cb7dff7aa
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2020-12-24 01:51:12 -0500

    test travis and hook mongodb

--- server.js
+++ server.js
@@ -29,7 +29,13 @@ app.use(passport.session());
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern", mongoOptions);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern", {
+	useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+	useFindAndModify: false,
+	mongoOptions
+});
 
 // Start the API server
 app.listen(PORT, function () {
