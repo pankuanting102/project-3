ef7e7063965c3bc06a7ea47a99320d69654ccf51
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2021-01-01 18:02:31 -0500

    deploying

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(passport.session());
 
 // Add routes, both API and view
 app.use(routes);
-
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+  }
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern", mongoOptions);
 
